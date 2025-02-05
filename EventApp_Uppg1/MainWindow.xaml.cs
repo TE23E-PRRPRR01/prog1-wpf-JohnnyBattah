37ba0134e5ea565ffb333456dbcd30099c61207b
Johnny Battah <johnny.battah@elev.ga.ntig.se>
2025-02-05 19:59:15 +0100

    Uppgift 1 2 och 3 av Eventhantering, App uppgifter

---
+++ EventApp_Uppg1/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EventApp_Uppg1;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+    private void KlickaRöd(object sender, RoutedEventArgs e)
+    {
+        this.Background = Brushes.Red;
+    }
+    private void KlickaGrön(object sender, RoutedEventArgs e)
+    {
+        this.Background = Brushes.Green;
+    }
+}
---
+++ EventApp_Uppg3/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EventApp_Uppg3;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+    private void KlickaÖkaBredd(object sender, RoutedEventArgs e)
+    {
+        this.Width += 10;
+    }
+    private void KlickaMinskaBredd(object sender, RoutedEventArgs e)
+    {
+       this.Width -= 10;
+    }
+    private void KlickaÖkaHöjd(object sender, RoutedEventArgs e)
+    {
+       this.Height += 10;
+    }
+    private void KlickaMinskaHöjd(object sender, RoutedEventArgs e)
+    {
+       this.Height -= 10;
+    }
+}
