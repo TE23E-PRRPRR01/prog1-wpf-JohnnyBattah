a5eba2c17f38f57f1e2823b7679b26834acc491b
Johnny Battah <johnny.battah@elev.ga.ntig.se>
2025-02-12 15:02:49 +0100

    Tränat på xaml o tryparse

---
+++ SlumpaApp/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SlumpaApp;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void KlickSlumpa(object sender, RoutedEventArgs e)
+    {
+        // Läs in maxvärdet från textboxen
+        // int max = int.Parse(txbMax.Text);
+
+        // Läsa av utan risk för krash
+        if (!int.TryParse(txbMax.Text, out int max)) // Vi ska läsa av texten i txbMax och variabeln ska hela max
+        {
+            max = 100;
+        }
+
+        int slumptal = Random.Shared.Next(1, max + 1);   
+        
+        txbResultat.Text = $"{slumptal}";
+    }
+}
