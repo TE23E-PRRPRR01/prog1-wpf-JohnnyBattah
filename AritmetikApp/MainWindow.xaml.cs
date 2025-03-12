d14321c4e7c2bc6e45062c18effbb653b8efbaf7
Johnny Battah <johnny.battah@elev.ga.ntig.se>
2025-03-12 20:54:52 +0100

    AritmetikApp

---
+++ AritmetikApp/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AritmetikApp;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+    private void KlickBeräkna(object sender, RoutedEventArgs e)
+    {
+        if (!int.TryParse(txbtal1.Text.Trim(), out int tal1))
+        {
+            lblResultat.Content = "Fel: Ogiltig inmatning för Tal 1";
+            return;
+        }
+        if (!int.TryParse(txbtal2.Text.Trim(), out int tal2))
+        {
+            lblResultat.Content = "Fel: Ogiltig inmatning för Tal 2";
+            return;
+        }
+        string Operator = txbOperator.Text.Trim();
+
+        if (Operator == "+")
+        {
+            lblResultat.Content = $"Resultat: {tal1 + tal2}";
+        }
+        if (Operator == "-")
+        {
+            lblResultat.Content = $"Resultat: {tal1 - tal2}";
+        }
+        if (Operator == "*")
+        {
+            lblResultat.Content = $"Resultat: {tal1 * tal2}";
+        }
+        if (Operator == "/")
+        {
+            lblResultat.Content = $"Resultat: {tal1 / tal2}";
+        }
+        else
+        {
+            lblResultat.Content = "Fel: Ogiltig Operator.";
+        }
+    }
+}
